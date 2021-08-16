6286169fb204c8201e1e4a948f47b4e2bdb79ca5
JIVitian <jivitian@gmail.com>
2021-08-16 16:14:52 -0300

    Probando cambiar los componentes del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Ivan') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rem sapiente ipsam temporibus animi iusto ipsum impedit sit omnis doloremque, id, vero, itaque aut quis perspiciatis nisi accusantium mollitia recusandae.</p>
+                <p>Aspernatur et quia nemo ab alias voluptate, reiciendis, at maiores, laboriosam placeat accusamus voluptas corporis odio molestias quidem consequatur incidunt! Maiores minus, ab harum vero ut ea eum dolorum atque!</p>
+                <p>Tenetur natus voluptas voluptate molestiae veniam ea reprehenderit maiores, consequatur labore nesciunt, accusantium nobis quos exercitationem, cumque error repellendus in tempore! Laudantium hic culpa, quasi pariatur quis saepe soluta tenetur!</p>
+                <p>Id excepturi magnam illum enim praesentium porro obcaecati. Iusto perferendis ducimus praesentium sapiente ab sed voluptate corrupti quam nisi, ea id atque reprehenderit quaerat nam architecto cum, vel deserunt saepe!</p>
+                <p>Eveniet, illum, beatae eligendi reprehenderit voluptas veritatis cumque vel quo cupiditate officiis temporibus rerum provident fugit laudantium omnis. Velit ex aliquid perferendis cumque quod quidem et! Nobis saepe accusamus voluptatibus?</p>
             </div>
         </div>
     </div>
