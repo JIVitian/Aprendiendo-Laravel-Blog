7914444d46e275ecf15ebe2ba0158e807887c52d
JIVitian <jivitian@gmail.com>
2021-08-17 11:58:30 -0300

    Utilizando Middlewares

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->user()->email == "jvitian@gmail.com") {
+            return $next($request);
+        }
+        else {
+            return redirect('no-autorizado');
+        }
+
+        // return redirect('/');
+    }
+}
